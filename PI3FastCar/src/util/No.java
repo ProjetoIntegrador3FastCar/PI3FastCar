1c808fa76d6bc70b1599b6f95079884fa47a30f7
Rodrigo Leite <30659350+Rodrigolmg@users.noreply.github.com>
2018-06-08 16:08:27 -0300

    Classe Viagem e utilitários

---
+++ PI3FastCar/src/util/No.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author rodri
+ */
+public class No<T> {
+    
+    private T objeto;
+    private No proximoNo;
+    
+    public No(T objeto, No proximoNo){
+        this.objeto = objeto;
+        this.proximoNo = proximoNo;
+    }
+
+    public No() {
+    }
+
+    public T getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(T objeto) {
+        this.objeto = objeto;
+    }
+
+    public No getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(No proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+}
