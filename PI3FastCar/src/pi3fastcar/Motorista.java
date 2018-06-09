b75180cae881724b364a697e407b2b8de11b9b12
kleitonpsld <kleitonpsld@hotmail.com>
2018-06-09 11:47:37 -0300

    Classes: Motorista e Veiculo

---
+++ PI3FastCar/src/pi3fastcar/Motorista.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pi3fastcar;
+
+/**
+ *
+ * @author Kleiton
+ */
+public class Motorista {
+    private int condigo_motorista;
+    private String nome;
+    private long cpf;
+
+    public Motorista(int condigo_motorista, String nome, long cpf) {
+        this.condigo_motorista = condigo_motorista;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Motorista() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getCondigo_motorista() {
+        return condigo_motorista;
+    }
+
+    public void setCondigo_motorista(int condigo_motorista) {
+        this.condigo_motorista = condigo_motorista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(long cpf)throws Exception {
+        if(cpf <= 0 )throw new Exception("O CPF não pode ser <= 0");
+        this.cpf = cpf;
+    }
+    
+    
+}
