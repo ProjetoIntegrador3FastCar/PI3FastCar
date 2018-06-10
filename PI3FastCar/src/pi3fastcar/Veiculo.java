37b0f29dc2624d106381546d5c35b3ae4fb3023a
kleitonpsld <kleitonpsld@hotmail.com>
2018-06-10 11:06:50 -0300

    Revert "Entidades: Motorista, Veiculo e validacao cpf"
    This reverts commit eefb424ba826a4370e655e7f4cc49761e5344c45.

---
+++ PI3FastCar/src/pi3fastcar/Veiculo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pi3fastcar;
+
+/**
+ *
+ * @author Kleiton
+ */
+public class Veiculo {
+    private int codigo_veiculo;
+    private String placa;
+    private String cor;
+    private String modelo;
+
+    public Veiculo(int codigo_veiculo, String placa, String cor, String modelo) {
+        this.codigo_veiculo = codigo_veiculo;
+        this.placa = placa;
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+
+    public int getCodigo_veiculo() {
+        return codigo_veiculo;
+    }
+
+    public void setCodigo_veiculo(int codigo_veiculo) {
+        this.codigo_veiculo = codigo_veiculo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
+}
